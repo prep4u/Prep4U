e261bbecf6ef4b5bccdcdcbde570d8ac5ae6f254
prep4u <prep4444u@gmail.com>
2026-09-26 14:37:39 +0545

    Update [[path]].js

--- function/[[path]].js
+++ function/[[path]].js
@@ -1,13 +1,26 @@
 export async function onRequest(context) {
     const url = new URL(context.request.url);
 
+    // 1. If someone requests a .html URL,
+    // redirect them to the clean URL.
+    if (url.pathname.toLowerCase().endsWith(".html")) {
+        const cleanUrl = new URL(url);
+
+        cleanUrl.pathname = cleanUrl.pathname.slice(0, -5);
+
+        return Response.redirect(cleanUrl.toString(), 302);
+    }
+
+    // 2. If someone requests a clean URL,
+    // internally load the corresponding .html file.
     if (
         url.pathname !== "/" &&
-        !url.pathname.includes(".") &&
-        !url.pathname.endsWith("/")
+        !url.pathname.endsWith("/") &&
+        !url.pathname.includes(".")
     ) {
         url.pathname += ".html";
     }
 
+    // 3. Serve the file.
     return context.env.ASSETS.fetch(url);
 }
